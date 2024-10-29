85ed8f19e6b76442ee44e6d88a0f985edbf6adfa
32Valik92 <strukalo.valentyn@gmail.com>
2024-10-29 14:21:27 +0200

    try deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
+// HomePage.tsx
 import "./page.css";
 
 import React from "react";
 
-import {BalanceInfoComponent, TransactionsListComponent} from "@/components";
+import { BalanceInfoComponent, TransactionsListComponent } from "@/components";
+import { transactionService } from "@/services";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const transactions = await transactionService.getAll();
+
   return (
-    <div className={"main-transactions-list"}>
-      <BalanceInfoComponent/>
+    <div className="main-transactions-list">
+      <BalanceInfoComponent />
 
-      <TransactionsListComponent/>
+      <TransactionsListComponent transactions={transactions} />
     </div>
   );
 }
--- src/components/server/TransactionsListComponent/TransactionsListComponent.tsx
+++ src/components/server/TransactionsListComponent/TransactionsListComponent.tsx
@@ -2,12 +2,14 @@ import "./TransactionsListComponent.css";
 
 import React from "react";
 
-import {TransactionComponent} from "@/components";
-import {transactionService} from "@/services";
+import { TransactionComponent } from "@/components";
+import { ITransactions } from "@/models";
 
-const TransactionsListComponent = async () => {
-  const transactions = await transactionService.getAll();
+interface TransactionsListProps {
+  transactions: ITransactions[];
+}
 
+const TransactionsListComponent: React.FC<TransactionsListProps> = ({ transactions }) => {
   return (
     <div className="transactions-list-component">
       <div className="transactions-list-header">
@@ -15,18 +17,15 @@ const TransactionsListComponent = async () => {
       </div>
 
       <div className="transactions-list">
-        {
-          transactions.map(transaction =>
-            <TransactionComponent
-              transaction={transaction}
-              key={transaction.id}
-            />
-          )
-        }
+        {transactions.map((transaction) => (
+          <TransactionComponent
+            transaction={transaction}
+            key={transaction.id}
+          />
+        ))}
       </div>
-
     </div>
   );
 };
 
-export {TransactionsListComponent};
+export { TransactionsListComponent };
